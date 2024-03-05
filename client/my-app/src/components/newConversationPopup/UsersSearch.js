5c8a218a3774ba05900adc055d31cee9cb7c11d1
Benjamin Capetillo <capetillobenjamin85@gmail.com>
2024-03-05 07:41:25 -0500

    Removes user's own address from list of users to message

--- client/my-app/src/components/newConversationPopup/UsersSearch.js
+++ client/my-app/src/components/newConversationPopup/UsersSearch.js
@@ -1,9 +1,12 @@
 import react from 'react';
 import UsersListItem from './UsersListItem.js';
-import { state, useState } from 'react';
+import { state, useState, useContext } from 'react';
+import { UserContext } from '../../UserWrapper.js';
 import '../../App.css';
 
 export default function UsersSearch({ addUser, searchForUsers, results }) {
+
+    const userContext = useContext(UserContext);
     const [ email, setEmail ] = useState('undefined');
 
     return(
@@ -17,7 +20,7 @@ export default function UsersSearch({ addUser, searchForUsers, results }) {
             }}>Search</button>
             <div className="results">
                 {results.length > 0 ? results.map(res => {
-                    return <UsersListItem user={res} addUser={addUser} />;
+                    return res._id !== userContext.user._id ? <UsersListItem user={res} addUser={addUser} /> : null;
                 }) : null }
             </div>
         </div>
